bec83ed555e31518f68ddeb474c782d8c9187919
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-11-02 00:41:23 +0200

    Remove import statements for characters

--- src/characters.ts
+++ src/characters.ts
@@ -1,50 +1,72 @@
 import { v4 as uuidv4 } from "uuid";
-import brook from "./assets/images/characters/brook.png";
-import chopper from "./assets/images/characters/chopper.png";
-import franky from "./assets/images/characters/franky.png";
-import jimbei from "./assets/images/characters/jimbei.png";
-import luffy from "./assets/images/characters/luffy.png";
-import nami from "./assets/images/characters/nami.png";
-import robin from "./assets/images/characters/robin.png";
-import sanji from "./assets/images/characters/sanji.png";
-import ussop from "./assets/images/characters/usopp.png";
-import zoro from "./assets/images/characters/zoro.png";
 
 type Character = {
+  /**
+   * Unique identifier for the character.
+   */
   id: string;
+  /**
+   * The name of the character.
+   */
   name: string;
+  /**
+   * The absolute path of the image of the character.
+   */
   imageSrc: string;
+  /**
+   * The boolean to indicate if the user has been selected.
+   */
   selected: boolean;
 };
 
 type CharacterImage = {
+  /**
+   * The absolute path of the image of the character.
+   */
   default: string;
 };
 
 const characters: Character[] = [];
 
+/**
+ * Retrieves the filename from the given path to the file.
+ * @param filePath The path to the file.
+ * @returns The file name without extension.
+ */
 function getFileName(filePath: string): string {
-  const pathParts = filePath.split("/"); // Split the path by '/'
-  const fileNameWithExtension = pathParts[pathParts.length - 1]; // Get the last part (the file name)
-  const fileName = fileNameWithExtension.split(".").slice(0, -1).join("."); // Remove the file extension
-  const capitalizedFileName =
-    fileName.charAt(0).toUpperCase() + fileName.slice(1); // Capitalize the name
-  return capitalizedFileName;
+  const pathParts: string[] = filePath.split("/");
+  const fileNameWithExtension: string = pathParts[pathParts.length - 1];
+  const fileName: string = fileNameWithExtension
+    .split(".")
+    .slice(0, -1)
+    .join(".");
+  return fileName;
 }
 
+/**
+ * Create and add a character to an array.
+ * @param image The image of the character.
+ */
+function addCharacterToArray(image: CharacterImage) {
+  const name: string = getFileName(image.default).toLowerCase();
+  characters.push({
+    id: uuidv4(),
+    name: name,
+    imageSrc: image.default,
+    selected: false,
+  });
+}
+
+/**
+ * Imports all character images from a folder and adds all
+ * the characters to an array.
+ */
 function createCharacters() {
   const images = import.meta.glob<CharacterImage>(
     "./assets/images/characters/*"
   );
   for (const path in images) {
-    images[path]().then((image) => {
-      characters.push({
-        id: uuidv4(),
-        name: getFileName(path),
-        imageSrc: image.default,
-        selected: false,
-      });
-    });
+    images[path]().then(addCharacterToArray);
   }
 }
 
