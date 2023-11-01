4e77fc92426bdc8d7c616b61326adc077708b6db
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-11-01 23:59:47 +0200

    Add dynamic imports

--- src/characters.ts
+++ src/characters.ts
@@ -17,13 +17,33 @@ type Character = {
   selected: boolean;
 };
 
+type CharacterImage = {
+  default: string;
+};
+
 const characters: Character[] = [];
 
+function getFileName(filePath: string): string {
+  const pathParts = filePath.split("/"); // Split the path by '/'
+  const fileNameWithExtension = pathParts[pathParts.length - 1]; // Get the last part (the file name)
+  const fileName = fileNameWithExtension.split(".").slice(0, -1).join("."); // Remove the file extension
+  const capitalizedFileName =
+    fileName.charAt(0).toUpperCase() + fileName.slice(1); // Capitalize the name
+  return capitalizedFileName;
+}
+
 function createCharacters() {
-  const images = import.meta.glob("./assets/images/characters/*");
+  const images = import.meta.glob<CharacterImage>(
+    "./assets/images/characters/*"
+  );
   for (const path in images) {
     images[path]().then((image) => {
-      console.log(path, image);
+      characters.push({
+        id: uuidv4(),
+        name: getFileName(path),
+        imageSrc: image.default,
+        selected: false,
+      });
     });
   }
 }
