72d1551a13c4778469f550adeabbbdb05eae69a4
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-11-02 02:04:53 +0200

    Add sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import { useEffect, useState } from "react";
 import loadingImage from "./assets/images/luffy-gear-5.gif";
+import backgroundSound from "./assets/sounds/overtaken_one_piece.mp3";
+
 import "./App.css";
 import Game from "./components/Game/Game";
 
+enum GameState {
+  Menu,
+  Playing,
+  GameOver,
+}
+
 function App() {
+  const backgroundAudio = new Audio(backgroundSound);
+
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [soundMuted, setSoundMuted] = useState<boolean>(false);
+  const [gameState, setGameState] = useState<GameState>(GameState.Menu);
 
   useEffect(() => {
-    const loadingInterval = setInterval(() => setIsLoading(false), 5000);
+    setTimeout(() => setIsLoading(false), 5000);
+  }, [isLoading, soundMuted]);
 
-    return () => clearInterval(loadingInterval);
-  }, []);
+  const startGame = () => {
+    setGameState(GameState.Playing);
+    backgroundAudio.currentTime = 0;
+    backgroundAudio.loop = true;
+    backgroundAudio.play();
+  };
   return (
     <>
       {isLoading && (
@@ -18,7 +35,7 @@ function App() {
           <img src={loadingImage} alt="Loading..." />
         </div>
       )}
-      {!isLoading && <Game />}
+      {!isLoading && <Game soundMuted={soundMuted} />}
     </>
   );
 }
--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from "react";
 import { characters } from "../../characters";
 import Card from "../Card/Card";
+import flipSound from "../../assets/sounds/conqueror_haki_luffy.mp3";
+
 import "./Game.css";
 
-function Game() {
+type GameProps = {
+  soundMuted: boolean;
+};
+
+function Game({ soundMuted }: GameProps) {
   const [cardsFlipped, setCardsFlipped] = useState<boolean>(false);
-  const flipCards = () => {
-    setCardsFlipped(true);
-  };
 
   useEffect(() => {
     if (cardsFlipped) {
-      const flipInterval = setInterval(() => setCardsFlipped(false), 1500);
-      return () => clearInterval(flipInterval);
+      setTimeout(() => setCardsFlipped(false), 1500);
     }
   }, [cardsFlipped]);
+
+  const flipCards = () => {
+    if (!cardsFlipped && !soundMuted) {
+      const flipAudio = new Audio(flipSound);
+      flipAudio.currentTime = 1;
+      flipAudio.play();
+      setTimeout(() => flipAudio.pause(), 3250);
+    }
+    setCardsFlipped(true);
+  };
+
   return (
     <div className="board">
       {characters.map((character) => (
