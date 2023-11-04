160e40bc44ae397febdd874e1c53b37331b02431
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-11-04 02:44:56 +0200

    Update menu content

--- src/components/GameStart/GameStart.tsx
+++ src/components/GameStart/GameStart.tsx
@@ -8,35 +8,24 @@ function GameStart({ handleStartGame }: GameStartProps): React.JSX.Element {
   return (
     <div className="menu-container">
       <div className="menu-modal">
-        <div className="menu-content">
-          <div>
-            <p>
-              Welcome to the world of One Piece! It's a beautiful day in the
-              Grand Line, but there's a problem. Noko the mischievous seahorse
-              has stolen your precious memories! Without your memories, you
-              won't remember the incredible adventures you've had with your
-              favorite One Piece characters, and you won't be able to save your
-              beloved nakamas.
-            </p>
-            <p>
-              But don't worry, because you have a chance to retrieve those
-              memories and reunite with your friends. In this thrilling memory
-              card game, you will choose a character in the given cards to piece
-              together your past and save your nakamas. However, there's a
-              catch: you can't choose the same character more than once. Each
-              card you match will bring you one step closer to restoring your
-              memories and rekindling the bonds with the Straw Hat Pirates and
-              other beloved characters.
-            </p>
-            <p>
-              Are you ready to embark on this unforgettable journey and reclaim
-              your lost memories? Let's set sail and save the day with the power
-              of friendship, adventure, and your love for One Piece!
-            </p>
+        <div className="content-container">
+          <div className="menu-content">
+            <div>
+              <p>
+                Your memories have been stolen by a mischievous seahorse named
+                Noko. Without them, you can't recall your adventures with One
+                Piece characters or save your friends. Play our memory card game
+                to match characters, piece your past together, and reunite with
+                your nakamas. But beware: you can't pick the same character
+                twice. Restore your memories, relive adventures, and strengthen
+                bonds with the Straw Hat Pirates. Ready to set sail and save the
+                day with friendship and adventure? Let's go!
+              </p>
+            </div>
+            <button className="btn" onClick={handleStartGame}>
+              Start Game
+            </button>
           </div>
-          <button className="btn" onClick={handleStartGame}>
-            Start Game
-          </button>
         </div>
       </div>
     </div>
