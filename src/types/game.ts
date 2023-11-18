0fb54cd62fb24641b0ebe8da6ad324a6ab892467
Thabo Vincent Moloi <102384087+thabomcodes@users.noreply.github.com>
2023-11-18 14:46:51 +0000

    Add game types

---
+++ src/types/game.ts
@@ -0,0 +1,37 @@
+/** Represents the state of the game. */
+enum GameState {
+  /** Game is at menu page. */
+  Menu,
+  /** Game is at start page. */
+  Start,
+  /** Game is playing. */
+  Playing,
+  /** The player has lost the game. */
+  GameOver,
+  /** The player has won the game. */
+  Victory,
+}
+
+/** Represents scores of the game. */
+interface Score {
+  /** Score of the game. */
+  score: number;
+  /** The best score. */
+  bestScore: number;
+  /** Increase the score by 1. */
+  incrementScore: () => void;
+  /** Increase the best score by 1. */
+  increaseBestScore: () => void;
+}
+
+/** Represents the background sound. */
+interface BackgroundSound {
+  /** Represents whether background sound is on/off */
+  muted: boolean;
+  /** Turns background sound off */
+  mute: () => void;
+  /** Turns background sound on */
+  unmute: () => void;
+}
+
+export type { GameState, Score, BackgroundSound };
